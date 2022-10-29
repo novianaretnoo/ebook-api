c54cafa8f4d1e20ba296002e4ff301a773d8b7b6
novianaretnoo <3103120170@student.smktelkom-pwt.sch.id>
2022-10-29 18:09:08 +0700

    challenge-author

--- e-book/routes/api.php
+++ e-book/routes/api.php
@@ -4,6 +4,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\BookController;
+use App\Http\Controllers\AuthorController;
 
 /*
 |--------------------------------------------------------------------------
@@ -29,5 +30,9 @@ Route::get('me', [AuthController::class, 'me']);
 // Route::delete('books{id}', [BookController::class, 'destroy']);
 
 Route::resource('books', BookController::class)->except(
+    ['create', 'edit']
+);
+
+Route::resource('authors', AuthorController::class)->except(
     ['create', 'edit']
 );
